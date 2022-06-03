9cc806542fc55d5fccd60160ab913284bac9ad31
Darren Ho <ho.yu.hsuan@gmail.com>
2022-06-03 09:58:06 +0800

    commit add Math class Branch

--- 0603/src/gitPkg/App.java
+++ 0603/src/gitPkg/App.java
@@ -4,7 +4,19 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ellie is little pig");
+		System.out.println("Ellie is little pig 1");
+		Math c = new Math();
+		System.out.println(c.add(10, 8));
 	}
 
+}
+
+class Math {
+	
+	int add(int a, int b) {
+		return a+b;
+	}
+	
+	
+	
 }
